e0753c80160d470ce46465ea739807bd28e7a5f5
Marta Santos <mdpesa@gmail.com>
2023-03-23 17:11:18 +0100

    add tests to convertPLNToUSD function

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,19 @@
 export const convertPLNToUSD = (PLN) => {
+  const parsedPLN = parseFloat(PLN);
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  }
+  if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  }
+  if (PLN < 0) {
+    return '$0.00';
+  }
 
-  const PLNtoUSD = PLN / 3.5;
-  
+  const PLNtoUSD = parsedPLN / 3.5;
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
   });
-
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+};
